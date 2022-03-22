9af2c9521c5f57b6355b97f64887a74aee813df0
brt9029 <brt9029@gmail.com>
2022-03-22 17:29:09 -0500

    added pages and components

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header';
+import Footer from './footer';
+
+function Home () {
+  return (
+    <main>
+      <Header />
+
+      <Footer />
+    </main>
+  )
+}
+
+export default Home;
