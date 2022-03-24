3adf92bee641eb1f5eb4b7974955d12a4dc8727b
brt9029 <brt9029@gmail.com>
2022-03-24 15:35:17 -0500

    adding log in modal

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -4,9 +4,9 @@ const MODAL_STYLES = {
   position: 'fixed',
   top: '50%',
   left: '50%',
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: '#FFF',
-  padding: '50px',
+  // transform: 'translate(-50%, -50%)',
+  backgroundColor: '#e9e9e9',
+  padding: '25px',
   zIndex: 1000
 }
 
@@ -15,8 +15,44 @@ function Login ({open, onClose}) {
 
   return (
     <div style={MODAL_STYLES}>
+      <div id="signup-modal" className="modal" style="display: block;">
+        <div className="modal-content container right-panel-active">
+          <div className="container_form container_signup">
+                <form className="form" id="form1">
+                  <h2 className="form_title">Join in on the fun!</h2>
+                  <input type="text" className="input" placeholder="Email" id="email-signup" autocomplete="off" />
+                  <input type="text" className="input" placeholder="Username" id="username-signup" autocomplete="off" />
+                  <input type="text" className="input" placeholder="Github" id="github-signup" autocomplete="off" />
+                  <input type="text" className="input" placeholder="Password" id="password-signup" autocomplete="off" />
+                    <button type="submit" className="auth-button" id="unique-btn" onClick={onClose}>Sign Up</button>
+                </form>
+            </div>
+
+            <div className="container_form container_signin">
+              <form className="form" id="form2">
+                <h2 className="form_title">Welcome Back!</h2>
+                <input type="text" className="input" placeholder="username" id="username-login" />
+                <input type="password" className="input" placeholder="password" id="password-login" />
+                <button type="submit" className="auth-button" id="log-btn">Log In</button>
+              </form>
+            </div>
+
+            <div className="container_overlay">
+              <div className="overlay">
+                <span className="close">x</span>
+                <div className="overlay_panel overlay_left">
+                  <button className="auth-button" id="login">Log In</button>
+                </div>
+                <div className="overlay_panel overlay_right">
+                  <button className="auth-button" id="signUp">Sign Up</button>
+                </div>
+              </div>
+            </div>
+            
+        </div>
+
+      </div>
       <button onClick={onClose}>Close Modal</button>
-      Login stuff here!
     </div>
   )
 }
