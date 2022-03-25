ad1047fb0de6d8a69c0188e03b3f6f0a8f7acb51
burcarz <zacharyburcar@gmail.com>
2022-03-25 17:49:02 -0400

    endless timeline component working, unable to query posts

--- client/src/components/Timeline/index.js
+++ client/src/components/Timeline/index.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from '@apollo/client';
+import { QUERY_ALL_POSTS } from '../../utils/queries';
+import { useAppContext } from '../../utils/GlobalState';
+import { UPDATE_TIMELINE } from '../../utils/actions';
 
 function Timeline() {
+    // const [state, dispatch] = useAppContext();
+
+    // // change posts shown in timeline with global state
+    // const { activeTimeline } = state;
+
+    const { loading, data } = useQuery(QUERY_ALL_POSTS);
+
+    console.log(data);
+    // useEffect(() => {
+    //     if (data) {
+    //         dispatch({
+    //             type: UPDATE_TIMELINE,
+    //             activeTimeline: data.posts
+    //         })
+    //     }
+    // }, [data])
+
     return (
-        <div>
-            this is timeline, posts go here
-        </div>
+        <section className="endless-scroll">
+            <section className="post-body">
+                <div type="button" className="up-vote-icon">
+                    <i type="upvote" className="fas fa-angle-up"></i>
+                </div>
+                <div className="title">
+                    <a href="single-post" target="blank">Post Link</a>
+                    <span>Posted By: <a href="/profile">Username</a> </span>
+                    <span> date created at </span>
+                    <span className="tags"><a href="tag_language">Tag Language</a>
+                    <a href="/tag_genre">Tag Genre</a></span>
+                </div>
+                <div className="body">
+                    <pre>
+                        <code>
+                            Post Body
+                        </code>
+                    </pre>
+                </div>
+                <div className="info">
+                    <a href="/post/{{id}}">Post LINK</a>
+                    <span className="hidden">id</span>
+                    <span className="vote-val" id="vote-val{{id}}">vote_count</span>
+                    <span className="vote-val"> votes</span>
+                </div>
+                <div type="button" className="down-vote-icon">
+                    <i type="downvote" className="fas fa-angle-down"></i>
+                </div>
+            </section>
+        </section>
     )
 }
 
