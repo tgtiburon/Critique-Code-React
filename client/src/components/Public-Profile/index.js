823163f6ddee61f2a9a68cad4bf9fb88471f756e
burcarz <zacharyburcar@gmail.com>
2022-03-27 22:39:08 -0400

    added code to public profile component

--- client/src/components/Public-Profile/index.js
+++ client/src/components/Public-Profile/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Profile from '../Profile';
 
 function PublicProfile() {
     return (
-        <div>
-            Public Profile
-        </div>
+    <section className="side-info">
+        <section className="sidebar-right">
+            <div className="inner-side">
+                <div className="header-card">
+                    <button className="edit-post auth-btns" id="edit-profile-btn">Edit</button>
+                            <h3 id="user-username"></h3>
+                        <div>
+                            <h4 id="title-user"></h4>
+                        </div>
+                <img className="profile-icon"></img>
+                </div>
+                <div className="about-section">
+                    <p id="github-id"></p>
+                    <p id="bio-text"></p>
+                </div>
+                <div>
+                    <div className="about-section">
+                        <p> posts <span> comments</span></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
     )
-}
+};
+
+export default PublicProfile;
