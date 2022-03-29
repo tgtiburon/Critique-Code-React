5bd766849cd5bc237b21011560b80e5af5713470
tgtiburon <tg.tiburon@gmail.com>
2022-03-29 14:14:56 -0400

    fixed auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -26,7 +26,7 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-      req.player = data;
+     // req.player = data;
     } catch {
       console.log("Invalid token");
     }
